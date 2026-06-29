ec585d47d662b12d161358b0003fc039839d056b
Zino1973 <zinner123@outlook.de>
2026-06-29 14:41:53 +0000

    Sitemap hinzugefügt

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.housevillatalija.de",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.housevillatalija.de/impressum",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
+    {
+      url: "https://www.housevillatalija.de/datenschutz",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
+  ];
+}
